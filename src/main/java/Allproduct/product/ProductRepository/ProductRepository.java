9b7c90f7ed2a005eaac66777162f7c1cc51c2167
esther306 <157009941+esther306@users.noreply.github.com>
2025-10-05 15:00:52 +0100

    Add files via upload

---
+++ src/main/java/Allproduct/product/ProductRepository/ProductRepository.java
@@ -0,0 +1,13 @@
+package Allproduct.product.ProductRepository;
+
+import Allproduct.product.Product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    List<Product> findProductById(Long id);
+}
